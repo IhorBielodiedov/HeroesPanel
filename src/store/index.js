f646f9885b9c17626aa91e17ce4260df2ef0351f
JanBadon <jan.badon.makaron@gmail.com>
2022-12-07 16:25:07 +0300

    ...

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import reducer from '../reducers';
+import ReduxThunk from 'redux-thunk';
+
+const stringMiddleware = () => (next) => (action) => {
+    if(typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+const store = createStore(combineReducers({heroes,filters}),
+                            compose(applyMiddleware(ReduxThunk, stringMiddleware),
+                                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+export default store;
